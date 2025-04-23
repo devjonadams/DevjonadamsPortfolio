82157ddbf45f19577256a91b5d0fde835b8a004b
DevJonAdams <Devjonadams@gmail.com>
2025-04-23 17:50:39 +0200

    Adds a bunch of training data
    Adds a bunch of filled-in Star-Wars training data.

---
+++ MarkovNameGen/src/main/java/com/devjonadams/markov/util/TrainingDataBuilder.java
@@ -0,0 +1,10 @@
+package com.devjonadams.markov.util;
+
+import java.util.ArrayList;
+
+public class TrainingDataBuilder {
+
+    public static ArrayList<String> build(String inDirectoryPath) {
+
+    }
+}
